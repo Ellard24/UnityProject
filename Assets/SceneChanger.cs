2ada1ecbace24619a15360ae054f734a42b81446
Ellard Gerritsen <egvdh@comcast.net>
2016-12-08 19:27:14 -0500

    Scene Changer initial work

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Platformer {
+
+  public class SceneChanger : MonoBehaviour {
+
+    public GameObject Player;
+
+    private static bool created = false;
+
+    public string currentScene;
+    public string nextScene;
+
+    void Awake() {
+      if (!created) {
+        DontDestroyOnLoad(this.gameObject);
+        created = true;
+      } else
+        Destroy(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start() {
+      Player = GameObject.Find("Player");
+      currentScene = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    /* 
+     * OnRenderObject gets called every time a scene loads and is much more useful
+     * than Update() when dealing with scene changes because it won't get called a million times
+     */
+    void OnRenderObject() {
+
+      // Identify the current scene by name
+      currentScene = SceneManager.GetActiveScene().name;
+    }
+
+    private void spawnPlayerInNewScene(GameObject Player, Vector2 PlayerSpawnPoint) {
+      // Debug.Log(PlayerSpawnPoint.transform.position.x);
+      // Debug.Log(PlayerSpawnPoint.transform.position.y);
+
+      Player.transform.position = PlayerSpawnPoint;
+    }
+
+
+
+
+  }
+}
