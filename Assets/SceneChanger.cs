091f22ccad4ee2d4670dab1d454aaab68eb6834f
Ellard Gerritsen <egvdh@comcast.net>
2016-12-10 13:49:11 -0500

    added scene changer. currently set to E

--- Assets/SceneChanger.cs
+++ Assets/SceneChanger.cs
@@ -9,10 +9,15 @@ namespace Platformer {
     public GameObject Player;
 
     private static bool created = false;
-
+      
     public string currentScene;
     public string nextScene;
+    private Vector2 playerCoords;
 
+    public KeyCode seasonChange = KeyCode.E;
+
+
+    //This will make sure we dont ever have more than one scene changer object
     void Awake() {
       if (!created) {
         DontDestroyOnLoad(this.gameObject);
@@ -30,6 +35,9 @@ namespace Platformer {
     // Update is called once per frame
     void Update() {
 
+      if (Input.GetKeyDown(seasonChange)) {
+        changeScene();
+      }
     }
     /* 
      * OnRenderObject gets called every time a scene loads and is much more useful
@@ -41,15 +49,34 @@ namespace Platformer {
       currentScene = SceneManager.GetActiveScene().name;
     }
 
-    private void spawnPlayerInNewScene(GameObject Player, Vector2 PlayerSpawnPoint) {
-      // Debug.Log(PlayerSpawnPoint.transform.position.x);
-      // Debug.Log(PlayerSpawnPoint.transform.position.y);
+    private void changeScene() {
+      playerCoords = Player.transform.position;
 
-      Player.transform.position = PlayerSpawnPoint;
+      //we will use a specific naming convention to streamline scene changes. 
+      //This way we can keep each set of scenes per level compartmentalized 
+      sceneNameChanger();
+      SceneManager.LoadScene(nextScene);
+      spawnPlayerInNewScene();
     }
 
+    private void spawnPlayerInNewScene() {
+      
+      //we will need to add some kind of checking system to ensure the player doesnt spawn in a bad location
+      Player.transform.position = playerCoords;
+    }
 
-
-
+    /*
+     * Takes the current scene name and creates the appropriate name for nextScene so 
+     * that the scene manager can appropriately change scenes
+     * */
+    private void sceneNameChanger() {
+      if (currentScene.Contains("Spring")) {
+        string[] strArr = currentScene.Split(new string[] {"Season"}, System.StringSplitOptions.None);
+        nextScene = string.Format("{0}SeasonWinter", strArr[0]);
+      }else if (currentScene.Contains("Winter")) {
+        string[] strArr = currentScene.Split(new string[] { "Season" }, System.StringSplitOptions.None);
+        nextScene = string.Format("{0}SeasonSpring", strArr[0]);
+      }
+    }
   }
 }
