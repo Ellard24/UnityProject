f24b7e1f1cf032f3a939b498e452bbb61495c892
Alli <ahoughton004@gmail.com>
2016-12-11 13:44:51 -0800

    Player Warp- Not working

---
+++ Assets/Scripts/PlayerWarp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Platformer
+{
+    public class PlayerWarp : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        //Warp player to coordinates 
+        void OnTriggerEnter2D(Collider other)
+        {
+            if (other.gameObject.CompareTag ("Player"))
+            {
+                other.transform.position = new Vector3(7, 2, 0);
+
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
